dc184b0c11332ad24cca164109cd5574598eee9e
LuciosGIT <jloliveirajunior1903@gmail.com>
2025-02-17 16:45:39 -0300

    feat  :  add redirection with Location import

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,3 +1,4 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -15,7 +16,8 @@ export class CourseFormComponent implements OnInit{
 
   constructor(private formBuilder: FormBuilder,
     private service: CoursesService,
-    private snackBar: MatSnackBar
+    private snackBar: MatSnackBar,
+    private location: Location
   ) {
 
     this.form = this.formBuilder.group({
@@ -32,7 +34,7 @@ export class CourseFormComponent implements OnInit{
   onSubmit() {
     this.service.save(this.form.value).subscribe({
 
-      next: (data) => console.log(data),
+      next: () => this.onSuccess(),
 
       error: () => {
         this.onError();
@@ -41,7 +43,12 @@ export class CourseFormComponent implements OnInit{
   }
 
   onCancel() {
+    this.location.back();
+  }
 
+  private onSuccess() {
+    this.snackBar.open('Curso salvo com sucesso.', '', { duration: 5000 });
+    this.onCancel();
   }
 
   private onError() {
